fed50ac0fc50d1057463a659a0167307a6892cc0
JohnKolapo <kollyp2000@outlook.com>
2025-05-07 05:07:51 -0700

    Update main.tf

--- evironments/dev/main.tf
+++ evironments/dev/main.tf
@@ -1,1 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "dns" {
+  name     = "rg-opsbydesign-dns"
+  location = "Canada Central"
+}
+
+resource "azurerm_dns_zone" "main" {
+  name                = "opsbydesign.ca"
+  resource_group_name = azurerm_resource_group.dns.name
+}
 
