b1b8fa50991962dd4bbe75b9f2d9dcc53d663b1e
JohnKolapo <kollyp2000@outlook.com>
2025-05-07 05:13:53 -0700

    Update variables.tf

--- evironments/dev/variables.tf
+++ evironments/dev/variables.tf
@@ -1,1 +1,20 @@
+variable "dns_zone_name" {
+  description = "The name of the DNS zone."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region."
+  type        = string
+}
+
+variable "records" {
+  description = "A map of DNS records."
+  type        = map(string)
+}
 
