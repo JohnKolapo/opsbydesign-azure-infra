28d39e1c59c7de7e42068fa5576c706adb9605e4
JohnKolapo <kollyp2000@outlook.com>
2025-05-07 05:13:13 -0700

    Update backend.tf

--- evironments/dev/backend.tf
+++ evironments/dev/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-opsbydesign-state"
+    storage_account_name = "opsbydesignstate"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
 
