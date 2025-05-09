f70d9f92ca72138cfcec8de7be3d7dc48e7eb242
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:12:04 -0700

    Update outputs.tf

--- functions/outputs.tf
+++ functions/outputs.tf
@@ -1,1 +1,12 @@
+# modules/functions/outputs.tf
+
+output "function_app_id" {
+  description = "ID of the Function App"
+  value       = azurerm_function_app.this.id
+}
+
+output "function_app_name" {
+  description = "Name of the Function App"
+  value       = azurerm_function_app.this.name
+}
 
