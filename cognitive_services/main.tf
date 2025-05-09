d1d90b6c85c7d25ddc1a08ac43700e8e9a7e192b
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:39:22 -0700

    Update main.tf

--- cognitive_services/main.tf
+++ cognitive_services/main.tf
@@ -1,1 +1,12 @@
+# modules/cognitive_services/main.tf
+
+resource "azurerm_cognitive_account" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kind                = var.kind
+  sku_name            = var.sku_name
+
+  tags = var.tags
+}
 
