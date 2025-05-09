9e691e20d11f8852984bd85f9ea275d1f56a2430
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:44:35 -0700

    Update main.tf

--- app_insights/main.tf
+++ app_insights/main.tf
@@ -1,1 +1,11 @@
+# modules/app_insights/main.tf
 
+resource "azurerm_application_insights" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = var.application_type
+  retention_in_days   = var.retention_in_days
+
+  tags = var.tags
+}
