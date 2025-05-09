cd388d6d7977610132a4820eb79d3e7e081f8ed0
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:34:26 -0700

    Update variables.tf

--- cosmosdb/variables.tf
+++ cosmosdb/variables.tf
@@ -1,1 +1,22 @@
+# modules/cosmosdb/variables.tf
 
+variable "name" {
+  description = "Name of the CosmosDB account"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+  default     = {}
+}
