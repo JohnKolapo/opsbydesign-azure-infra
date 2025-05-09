a2693c0f3371c59112d7977a11e80a07c4b7b963
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:40:30 -0700

    Update outputs.tf

--- cognitive_services/outputs.tf
+++ cognitive_services/outputs.tf
@@ -1,1 +1,12 @@
+# modules/cognitive_services/outputs.tf
+
+output "cognitive_account_id" {
+  description = "ID of the Cognitive Services account"
+  value       = azurerm_cognitive_account.this.id
+}
+
+output "cognitive_account_name" {
+  description = "Name of the Cognitive Services account"
+  value       = azurerm_cognitive_account.this.name
+}
 
