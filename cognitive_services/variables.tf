b7ec9239c636ce7dcd1b97a49717816acff97933
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:40:03 -0700

    Update variables.tf

--- cognitive_services/variables.tf
+++ cognitive_services/variables.tf
@@ -1,1 +1,34 @@
+# modules/cognitive_services/variables.tf
 
+variable "name" {
+  description = "Name of the Cognitive Services account"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "kind" {
+  description = "Type of cognitive service (e.g., CognitiveServices, OpenAI)"
+  type        = string
+  default     = "CognitiveServices"
+}
+
+variable "sku_name" {
+  description = "SKU (e.g., S0, S1)"
+  type        = string
+  default     = "S0"
+}
+
+variable "tags" {
+  description = "Tags for the resource"
+  type        = map(string)
+  default     = {}
+}
