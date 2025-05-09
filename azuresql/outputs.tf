a7fcb04a5e7214fc274c7d3d80af589fe7709793
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:37:50 -0700

    Update outputs.tf

--- azuresql/outputs.tf
+++ azuresql/outputs.tf
@@ -1,1 +1,12 @@
+# modules/azuresql/outputs.tf
+
+output "sql_server_name" {
+  description = "SQL Server name"
+  value       = azurerm_mssql_server.this.name
+}
+
+output "sql_database_name" {
+  description = "SQL Database name"
+  value       = azurerm_mssql_database.this.name
+}
 
