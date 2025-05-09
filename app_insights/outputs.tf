04f9542ad7fc578c6a0c271b893e02d92be398d5
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:45:32 -0700

    Update outputs.tf

--- app_insights/outputs.tf
+++ app_insights/outputs.tf
@@ -1,1 +1,12 @@
+# modules/app_insights/outputs.tf
+
+output "app_insights_id" {
+  description = "ID of the App Insights instance"
+  value       = azurerm_application_insights.this.id
+}
+
+output "app_insights_instrumentation_key" {
+  description = "Instrumentation key"
+  value       = azurerm_application_insights.this.instrumentation_key
+}
 
