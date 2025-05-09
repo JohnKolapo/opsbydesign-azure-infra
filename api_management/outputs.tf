544a94030c9226b9b2a8b53ad814c6165d60836b
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:17:25 -0700

    Update outputs.tf

--- api_management/outputs.tf
+++ api_management/outputs.tf
@@ -1,1 +1,12 @@
+# modules/api_management/outputs.tf
+
+output "api_management_id" {
+  description = "ID of the API Management instance"
+  value       = azurerm_api_management.this.id
+}
+
+output "api_management_name" {
+  description = "Name of the API Management instance"
+  value       = azurerm_api_management.this.name
+}
 
