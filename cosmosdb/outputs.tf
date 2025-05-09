8ff84bf16a2dca8fed1caaab162d29e1062d789c
JohnKolapo <kollyp2000@outlook.com>
2025-05-08 22:34:49 -0700

    Update outputs.tf

--- cosmosdb/outputs.tf
+++ cosmosdb/outputs.tf
@@ -1,1 +1,11 @@
+# modules/cosmosdb/outputs.tf
 
+output "cosmosdb_account_name" {
+  description = "CosmosDB account name"
+  value       = azurerm_cosmosdb_account.this.name
+}
+
+output "cosmosdb_account_id" {
+  description = "CosmosDB account ID"
+  value       = azurerm_cosmosdb_account.this.id
+}
